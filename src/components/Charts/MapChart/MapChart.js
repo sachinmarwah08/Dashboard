e5ee47d04fbd25b894a4947f29698313432f5fda
sachinmarwah08 <sachin.marwah@itday.in>
2022-08-01 22:08:05 +0530

    initial commit

--- src/components/Charts/MapChart/MapChart.js
+++ src/components/Charts/MapChart/MapChart.js
@@ -3,8 +3,8 @@ import "./MapChart.scss";
 import WorldMap from "../../../Images/WorldMap.svg";
 import Table from "../../../Images/Table.png";
 import BigArrow from "../../../Images/bif-Arrow.png";
-import { MapContainer, CircleMarker, TileLayer, Tooltip } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
+// import { MapContainer, CircleMarker, TileLayer, Tooltip } from "react-leaflet";
+// import "leaflet/dist/leaflet.css";
 import data from "./Cities";
 import GoogleMap from "../../googleMap";
 // import { Chart } from "react-google-charts";
@@ -20,12 +20,12 @@ import GoogleMap from "../../googleMap";
 // ];
 
 const MapChartComponent = () => {
-  var centerLat = (data.minLat + data.maxLat) / 2;
-  var distanceLat = data.maxLat - data.minLat;
-  var bufferLat = distanceLat * 0.05;
-  var centerLong = (data.minLong + data.maxLong) / 2;
-  var distanceLong = data.maxLong - data.minLong;
-  var bufferLong = distanceLong * 0.05;
+  // var centerLat = (data.minLat + data.maxLat) / 2;
+  // var distanceLat = data.maxLat - data.minLat;
+  // var bufferLat = distanceLat * 0.05;
+  // var centerLong = (data.minLong + data.maxLong) / 2;
+  // var distanceLong = data.maxLong - data.minLong;
+  // var bufferLong = distanceLong * 0.05;
   return (
     <div className="wrapper">
       <div className="content">
